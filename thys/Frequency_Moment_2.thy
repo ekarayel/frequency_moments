a98f3e79e48a1426d0978908b82fe5777b53a97b
Emin Karayel <me@eminkarayel.de>
2021-06-03 21:33:33 +0200

    indep_var_reindex

--- thys/Frequency_Moment_2.thy
+++ thys/Frequency_Moment_2.thy
@@ -34,12 +34,38 @@ fun counts where
 
 lemma c4: 
   assumes "x \<in> maps_inj n A"
-  shows "integral\<^sup>L \<Omega> (f2_tr h xs x a) = prod_list (map (\<lambda>(i,j). (LINT \<omega>|\<Omega>. (f2_sketch_summand_pow h xs (the (x i)) j \<omega>))) (counts a))"
+  shows "integral\<^sup>L \<Omega> (f2_tr h xs x a) =
+    prod_list (map (\<lambda>(i,j). (LINT \<omega>|\<Omega>. (f2_sketch_summand_pow h xs (the (x i)) j \<omega>))) (counts a))"
   sorry
 
-fun sm_l where
-  "sm_l [] _ = True" |
-  "sm_l (a#as) n = (if a < n then sm_l as n else False)"
+lemma (in prob_space) indep_sets_reindex:
+  assumes "inj_on f I"
+  shows "indep_sets F (f ` I) = indep_sets (F \<circ> f) I"
+proof -
+  have "indep_sets F (f ` I) \<Longrightarrow> indep_sets (F \<circ> f) I"
+  proof (rule indep_setsI)
+    show "\<And>i. indep_sets F (f ` I) \<Longrightarrow> i \<in> I \<Longrightarrow> (F \<circ> f) i \<subseteq> events" 
+      by (simp add: indep_sets_def)+
+  next
+    fix A
+    fix J
+    assume "indep_sets F (f ` I)"
+    assume "J \<subseteq> I"
+    hence c1:"f ` J \<subseteq> f ` I" by blast
+    assume "J \<noteq> {}"
+    hence c2:"f ` J \<noteq> {}" by blast
+    assume "finite J"
+    hence c3:"finite (f ` J)" sorry
+    assume "\<forall>j\<in>J. A j \<in> (F \<circ> f) j"
+    hence c4:"\<forall>j \<in> (f ` J). A (f j) \<in> F j" sorry
+    show "prob (\<Inter> (A ` J)) = (\<Prod>j\<in>J. prob (A j))" sorry
+  qed
+qed
+
+lemma (in prob_space) indep_vars_reindex:
+  assumes "indep_vars M' X1 (f ` I)"
+  shows "indep_vars (M' \<circ> f) (X1 \<circ> f) I"
+  using assms by (simp add: indep_vars_def2 indep_sets_reindex comp_def)
 
 lemma
   assumes "prob_space \<Omega>"
@@ -61,7 +87,22 @@ proof -
   have d:"Sigma_Algebra.measure \<Omega> (space \<Omega>) = 1" using assms(1) 
     by (simp add: prob_space.prob_space)
 
-  have e: "\<And>x n a. x \<in> maps n (set xs) \<Longrightarrow> sm_l a n \<Longrightarrow> integrable \<Omega> (f2_tr h xs x a)" sorry
+  have e: "\<And>x n a. x \<in> maps n (set xs) \<Longrightarrow> filter (\<lambda>i. i \<ge> n) a = [] \<Longrightarrow> integrable \<Omega> (f2_tr h xs x a)" sorry
+
+  have indep1:
+    "\<And>x n. x \<in> maps_inj n (set xs) \<Longrightarrow> n \<le> 4 \<Longrightarrow> prob_space.indep_vars \<Omega> (\<lambda>_. borel) (\<lambda>i. h (the (x i))) {k. k < n}"
+  proof -
+    fix x n
+    assume "x \<in> maps_inj n (set xs)"
+    assume "n \<le> 4"
+    show "prob_space.indep_vars \<Omega> (\<lambda>_. borel) (\<lambda>i. h (the (x i))) {k. k < n}"
+      using assms(3)[where I="(\<lambda>i. the (x i)) ` {k. k < n}"]
+
+  qed
+
+  have indep:
+    "\<And>x n j. x \<in> maps_inj n (set xs) \<Longrightarrow> n \<le> 4 \<Longrightarrow> prob_space.indep_vars \<Omega> (\<lambda>_. borel) (\<lambda>i. f2_sketch_summand_pow h xs (the (x i)) (j i)) {k. k < n}"   
+    sorry
 
   show ?A
     apply (simp add: f2_sketch_def power4_eq_xxxx power2_eq_square)
